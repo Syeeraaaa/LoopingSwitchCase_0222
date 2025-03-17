04d4bfbeca3e6c2865babbb4392e8863960b63d2
Syeeraaaa <syeerasilvia@gmai.com>
2025-03-17 20:46:56 +0700

    membuat menu menggunakan do while

--- MembuatSwitchCase.cpp
+++ MembuatSwitchCase.cpp
@@ -13,4 +13,16 @@ int main (){
     int pilihan;
     float Panjang, lebar;
     float jejari;
+
+    do {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
+    }
 }
