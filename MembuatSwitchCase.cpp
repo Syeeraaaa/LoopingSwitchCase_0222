37f613702530e6f0cd31c8cb2b0998c46c673829
Syeeraaaa <syeerasilvia@gmai.com>
2025-03-17 20:28:26 +0700

    membuat Fungsi PersegiPanjang dengan dua parameter

--- MembuatSwitchCase.cpp
+++ MembuatSwitchCase.cpp
@@ -1,1 +1,11 @@
-#include <iostream>s
+#include <iostream>
+using namespace std;
+
+float LuasPersegiPanjang(float a, float b){
+    return a*b;
+}
+
+ float LuasLingkaran (float a){
+    return 3.14*a*a;
+    
+ }
